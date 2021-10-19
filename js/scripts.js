c3795831f2c9de59535610ee38abc7171ca06bc5
mellowmorgan <morgan.waites@gmail.com>
2021-10-19 14:32:13 -0700

    Add three terms clickable jquery
    Co-authored-by: Kim Brannian <brannian@gmail.com>
    Co-authored-by: Liz Thomas <thomas.elizabeth.k@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $(".clickable").click(function(){
+    $(".def-hidden").toggle();
+    $(".def-showing").toggle();
+
+  });
+  $(".clickable-2").click(function(){
+    $(".def-hidden-2").toggle();
+    $(".def-showing-2").toggle();
+
+  });
+  $(".clickable-3").click(function(){
+    $(".def-hidden-3").toggle();
+    $(".def-showing-3").toggle();
+
+  });
+
+});
