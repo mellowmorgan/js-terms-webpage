a6ac53b90758e241233ea86e4fa0fe42a1d8f64e
mellowmorgan <morgan.waites@gmail.com>
2021-10-19 15:10:24 -0700

    Add css webpage class
    Co-authored-by: Kim Brannian <brannian@gmail.com>
    Co-authored-by: Liz Thomas <thomas.elizabeth.k@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,28 @@
 $(document).ready(function(){
   $(".clickable").click(function(){
-    $(".def-hidden").toggle();
+   
     $(".def-showing").toggle();
 
   });
   $(".clickable-2").click(function(){
-    $(".def-hidden-2").toggle();
+    // $(".def-hidden-2").toggle();
     $(".def-showing-2").toggle();
 
   });
   $(".clickable-3").click(function(){
-    $(".def-hidden-3").toggle();
+    // $(".def-hidden-3").toggle();
     $(".def-showing-3").toggle();
 
   });
 
   $(".clickable-4").click(function(){
-    $("#def-hidden-4").toggle();
+    // $("#def-hidden-4").toggle();
     $("#def-showing-4").toggle();
 
+  });
+
+  $(".clickable-5").click(function(){
+      // $("#def-hidden-5").toggle();
+      $("#def-showing-5").toggle();
   });
 });
